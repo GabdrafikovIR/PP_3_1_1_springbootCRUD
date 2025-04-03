3ab967b9480533b145fa443bf17709e3582629fc
Ildar Gabdrafikov <irgabdrafikov@yandex.ru>
2025-04-03 22:44:15 +0300

    Done!

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,65 @@
+package web.model;
+
+import jakarta.persistence.*;
+import org.hibernate.validator.constraints.NotEmpty;
+
+
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="name")
+    @NotEmpty(message = "Name should not be empty")
+    private String name;
+
+    @Column(name="lastname")
+    @NotEmpty(message = "Name should not be empty")
+    private String lastName;
+
+    public User() {
+    }
+
+
+
+    public User(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
